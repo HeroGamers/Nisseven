5f10a42a1ecab158f9fbfca74ff7fbaa424f70e2
rfoldbirk <rasmusfoldberg@gmail.com>
2020-08-19 13:37:58 +0200

    Fiksede fejl, som gjorde at man ikke kunne besøge webstedet. Opdelte porten i https og http

--- scripts/load.js
+++ scripts/load.js
@@ -4,23 +4,24 @@ const { readFileSync, writeFileSync, unlinkSync, mkdirSync, existsSync } = requi
 // disse indstillinger bør ikke ændres på...
 // btw selve settings.json er meget streng! Ryk rundt på det mindste og den nulstiller automatisk
 const defaultSettings = {
-	port: 8080,
-	useHttps: false,
-	forceSecureDBSave: false,
-	prettyPrint: true,
-	compileSassOnRun: false,
-	clearOnRun: true,
-	msgOnServerStart: false,
-	databases: ['users'],
-	superAdmins: ['rasmus', 'marcus']
+    https_port: 443,
+    http_port: 80,
+    useHttps: false,
+    forceSecureDBSave: false,
+    prettyPrint: true,
+    compileSassOnRun: false,
+    clearOnRun: true,
+    msgOnServerStart: false, // du kan eventuelt skrive beskeden her :)
+    databases: ['users'],
+    superAdmins: ['rasmus']
 }
 
 
 let defaultSettingsLength = 0
 let defaultSettingsKey = []
 for (let key in defaultSettings) {
-	defaultSettingsLength ++
-	defaultSettingsKey.push(key)
+    defaultSettingsLength ++
+    defaultSettingsKey.push(key)
 }
 
 
@@ -32,41 +33,41 @@ let crypto = require('crypto')
 const folders = ['config', 'config/collections']
 
 for (let folder of folders) {
-	if (!existsSync('./' + folder)) {
+    if (!existsSync('./' + folder)) {
 		mkdirSync('./' + folder)
-	}
+    }
 }
 
 
 
 function loadSettings() {
 	try {
-		global.port = 8080 // bare som en default
+        global.port = 8080 // bare som en default
 
-		const settings = JSON.parse(readFileSync(settingsPath))
+        const settings = JSON.parse(readFileSync(settingsPath))
 
-		let iterations = 0
+        let iterations = 0
 
-		for (let key in settings) {
-			if (defaultSettingsKey[iterations] != key) throw 'reset'
-			global[key] = settings[key]
+        for (let key in settings) {
+            if (defaultSettingsKey[iterations] != key) throw 'reset'
+            global[key] = settings[key]
 
-			iterations ++
-		}
+            iterations ++
+        }
 
-		if (iterations != defaultSettingsLength) throw 'reset'
-	}
-	catch (err) {
+        if (iterations != defaultSettingsLength) throw 'reset'
+    }
+    catch (err) {
 
-		if (err == 'reset') {
-			console.log('settings.json blev nulstillede...')
-			unlinkSync(settingsPath)
-		}
+        if (err == 'reset') {
+            console.log('settings.json blev nulstillede...')
+            unlinkSync(settingsPath)
+        }
 
-		writeFileSync('./config/settings.json', JSON.stringify(defaultSettings, null, 2))
+        writeFileSync('./config/settings.json', JSON.stringify(defaultSettings, null, 2))
 
-		loadSettings()
-	}
+        loadSettings()
+    }
 }
 
 loadSettings()
@@ -75,67 +76,45 @@ if (global.clearOnRun) console.clear()
 
 
 if (global.compileSassOnRun) {
-	const { exec } = require('child_process')
-	
-	console.log('* Compiling sass to css')
+    const { exec } = require('child_process')
 
-	exec('sass ./public/sass:./public/css', (err) => {
-		console.log('* Done compiling sass')
-	})
+    console.log('* Compiling sass to css')
+
+    exec('sass ./public/sass:./public/css', (err) => {
+        console.log('* Done compiling sass')
+    })
 }
 
 
 
 
 global.randomStr = (length) => {
-	var result           = ''
-	var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-	var charactersLength = characters.length
-	for ( var i = 0; i < length; i++ ) {
-		 result += characters.charAt(Math.floor(Math.random() * charactersLength))
-	}
-	return result
+    var result           = ''
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    var charactersLength = characters.length
+    for ( var i = 0; i < length; i++ ) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    return result
 }
 
 
 global.genCookie = (hours) => {
-	let cookie = global.randomStr(256)
-	let hashedCookie = global.hash(cookie)
+    let cookie = global.randomStr(256)
+    let hashedCookie = global.hash(cookie)
 
-	hours = hours || 1
+    hours = hours || 1
 
-	// cookieen udløber efter 1 timer
-	let expireTime = hours*60*60*1000 + Date.now()
+    // cookieen udløber efter 1 timer
+    let expireTime = hours*60*60*1000 + Date.now()
 
-	let cookieToStore = {
-		'cookie': hashedCookie,
-		'expireTime': expireTime
-	}
+    let cookieToStore = {
+        'cookie': hashedCookie,
+        'expireTime': expireTime
+    }
 
-	return {
-		realCookie: cookie,
-		cookieToStore: cookieToStore
-	}
-}
-
-
-global.hash = (str) => {
-	const salt = String( crypto.randomBytes(128).toString('base64') )
-	const iterations = 10000
-	const keylen = 256
-	const digest = 'sha512'
-
-	return {
-		salt: salt,
-		iterations: iterations,
-		hash: crypto.pbkdf2Sync(str, salt, iterations, keylen, digest).toString('base64'),
-		keylen: keylen,
-		digest: digest
-	}
-}
-
-
-global.verify = (str, hashObj) => {
-	let newHash = crypto.pbkdf2Sync(str, hashObj.salt, hashObj.iterations, hashObj.keylen, hashObj.digest).toString('base64')
-	return hashObj.hash == newHash
+    return {
+        realCookie: cookie,
+        cookieToStore: cookieToStore
+    }
 }
