3c653f7564c8db380c01a3cc542846c92cbec8c1
fido2603 <fido2603gamer@gmail.com>
2020-08-24 13:23:16 +0200

    stuffs

--- routes/auth.js
+++ routes/auth.js
@@ -12,10 +12,27 @@ router.post('/login', (req, res) => {
 		}
 
 		let user = global.users.findOne(req.body.username)
+		let today = new Date()
+		let dd = String(today.getDate()).padStart(2, '0')
+		let mm = String(today.getMonth() + 1).padStart(2, '0')
 
 		if (!user) throw 'bruger findes ikke'
 		if (!user.hasOwnProperty('password') || (user.hasOwnProperty('allowNewPassword') && user.allowNewPassword)) throw 'lav bruger'
 		if (!global.verify(req.body.password, user.password)) throw 'psw matcher ikke'
+		if ((dd + "/" + "mm") === "01/12") {
+			if (!user.hasOwnProperty("nisseven")) {
+				let users = global.users.collection()
+				for (let i = 0; i < users.length; i++) {
+					if (!users[i].distributed && users[i].id !== user.id) {
+						let nisseven = users[i]
+
+						global.users.updateOne(nisseven.id, {distributed: true})
+						let hashednisseven = nisseven
+						global.users.updateOne(user.id, {nisseven: hashednisseven})
+					}
+				}
+			}
+		}
 
 		let cookieObj = global.genCookie(1)
 
@@ -42,6 +59,7 @@ router.post('/login', (req, res) => {
 					password: global.hash(req.body.password),
 					cookie: cookieObj.cookieToStore,
 					nisseven: null,
+					distributed: false,
 					activated: Date.now(),
 					subscribed: false,
 					allowNewPassword: undefined
--- scripts/load.js
+++ scripts/load.js
@@ -91,6 +91,13 @@ global.verify = (str, hashObj) => {
     return hashObj.hash == newHash
 }
 
+global.encrypt = (str, key) => {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') }
+}
+
 
 global.hash = (str) => {
     const salt = String(  crypto.randomBytes(128).toString('base64') )
